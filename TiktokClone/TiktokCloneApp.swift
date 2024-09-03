5e7960899e823405dfc3cf7b1dcb7c3fbaf40e59
Wing Hu <terranzmczmx@gmail.com>
2024-09-03 15:32:57 -0700

    Initial Commit

---
+++ TiktokClone/TiktokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokCloneApp.swift
+//  TiktokClone
+//
+//  Created by Yingzhe Hu on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TiktokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
