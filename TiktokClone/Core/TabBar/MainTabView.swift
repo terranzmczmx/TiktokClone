9b825e764bc7a5c81944c0e25fbe39c20832ba27
Wing Hu <39383762+terranzmczmx@users.noreply.github.com>
2024-09-03 18:37:34 -0700

    update tabview

--- TiktokClone/Core/TabBar/MainTabView.swift
+++ TiktokClone/Core/TabBar/MainTabView.swift
@@ -15,36 +15,61 @@ struct MainTabView: View {
             Text("Feed")
                 .tabItem {
                     VStack {
-                        Image(systemName: "house")
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
                         Text("Home")
                     }
                 }
+                .onAppear {
+                    selectedTab = 0
+                }
+                .tag(0)
+            
             Text("Friends")
                 .tabItem {
                     VStack {
-                        Image(systemName: "person.2")
+                        Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
                         Text("Friends")
                     }
                 }
+                .onAppear {
+                    selectedTab = 1
+                }
+                .tag(1)
+            
             Text("Upload Post")
                 .tabItem {
                     Image(systemName: "plus")
                 }
+            
             Text("Notifications")
                 .tabItem {
                     VStack {
-                        Image(systemName: "heart")
+                        Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
                         Text("Inbox")
                     }
                 }
+                .onAppear {
+                    selectedTab = 3
+                }
+                .tag(3)
+            
             Text("Profile")
                 .tabItem {
                     VStack {
                         Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
                         Text("Profile")
                     }
                 }
+                .onAppear {
+                    selectedTab = 4
+                }
+                .tag(4)
         }
+        .tint(.black)
     }
 }
 
