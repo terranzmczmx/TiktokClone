973c6eefc84843357ff5191493810cb319bdc38f
Wing Hu <39383762+terranzmczmx@users.noreply.github.com>
2024-09-04 23:19:58 -0700

    Implement the Feed page

---
+++ TiktokClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,101 @@
+//
+//  FeedCell.swift
+//  TiktokClone
+//
+//  Created by Yingzhe Hu on 9/4/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    let post: Int
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.pink)
+                .containerRelativeFrame([.horizontal, .vertical])
+                .overlay {
+                    Text("Post \(post)")
+                        .foregroundStyle(.white)
+            }
+            
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("User Name")
+                            .fontWeight(.semibold)
+                        Text("Content")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            
+                        } label: {
+                            
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                Text("2051")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 22, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                .padding(.bottom, 80)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 27)
+}
